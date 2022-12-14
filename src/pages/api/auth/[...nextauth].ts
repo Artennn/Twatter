02187ba3c95e778c4c138849be4ed8e8df2ac56e
Artennn <119894750+Artennn@users.noreply.github.com>
2022-12-14 22:59:06 +0100

    feat: auth

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,28 +1,155 @@
 import NextAuth, { type NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google"
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import CredentialProvider from "next-auth/providers/credentials";
 
-import { env } from "../../../env/server.mjs";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { prisma } from "../../../server/db/client";
 
-export const authOptions: NextAuthOptions = {
-  // Include user.id on session
-  callbacks: {
-    session({ session, user }) {
-      if (session.user) {
-        session.user.id = user.id;
-      }
-      return session;
-    },
-  },
-  // Configure one or more authentication providers
-  adapter: PrismaAdapter(prisma),
-  providers: [
-    GoogleProvider({
-      clientId: env.GOOGLE_CLIENT_ID,
-      clientSecret: env.GOOGLE_CLIENT_SECRET,
-    }),
-  ],
-};
+import { env } from "../../../env/server.mjs";
+import { NextApiRequest, NextApiResponse } from "next";
+import { decode, encode } from "next-auth/jwt";
 
-export default NextAuth(authOptions);
+import { randomUUID } from "crypto";
+import Cookies from "cookies";
+
+declare module "next-auth" {
+    interface User {
+        profileID?: number,
+    }
+    interface Session {
+        // known issue
+        //@ts-ignore
+        user: {
+            authID: string,
+            profileID?: number,
+        }
+    }
+}
+
+const CREDENTIALS_SESSION_MAX_AGE = 60 * 60 * 24 * 30; // 30 days
+
+export const handler = async (pReq: NextApiRequest, pRes: NextApiResponse) => {
+    const [req, res, options] = requestWrapper(pReq, pRes);
+    return await NextAuth(req, res, options);
+};
+export default handler;
+
+const checkIfCredentials = (req: NextApiRequest) => 
+    req.query.nextauth?.includes("callback") &&
+    req.query.nextauth?.includes("credentials") &&
+    req.method === "POST"
+
+export function requestWrapper(
+    req: NextApiRequest,
+    res: NextApiResponse
+): [req: NextApiRequest, res: NextApiResponse, opts: NextAuthOptions] {
+    const generateSessionToken = () => randomUUID();
+
+    const fromDate = (time: number, date = Date.now()) =>
+        new Date(date + time * 1000);
+
+    const adapter = PrismaAdapter(prisma);
+
+    const opts: NextAuthOptions = {
+        pages: {
+            signIn: "/login",
+        },
+        adapter: adapter,
+        callbacks: {
+            session({ session, user }) {
+                //console.log({session, user})
+                if (session.user) {
+                    session.user = {
+                        authID: user.id,
+                        profileID: user.profileID,
+                    }
+                }
+                return session;
+            },
+            async signIn({ user, account, profile, email, credentials }) {
+                // Check if this sign in callback is being called in the credentials authentication flow. 
+                // If so, use the next-auth adapter to create a session entry in the database 
+                // (SignIn is called after authorize so we can safely assume the user is valid and already authenticated).
+                if (checkIfCredentials(req)) {
+                    if (user) {
+                        const sessionToken = generateSessionToken();
+                        const sessionMaxAge = CREDENTIALS_SESSION_MAX_AGE;
+                        const sessionExpiry = fromDate(sessionMaxAge);
+
+                        await adapter.createSession({
+                            sessionToken: sessionToken,
+                            userId: user.id,
+                            expires: sessionExpiry,
+                        });
+
+                        const cookies = new Cookies(req, res);
+                        cookies.set("next-auth.session-token", sessionToken, {
+                            expires: sessionExpiry,
+                        });
+                    }
+                }
+                if (account?.provider === "google") {
+                    return profile?.email?.endsWith("@pollub.edu.pl")? true : false
+                }
+                return true;
+            },
+        },
+        jwt: {
+            encode: async ({ token, secret, maxAge }) => {
+                if (checkIfCredentials(req)) {
+                    const cookies = new Cookies(req, res);
+                    const cookie = cookies.get("next-auth.session-token");
+                    if (cookie) return cookie;
+                    else return "";
+                }
+                // Revert to default behaviour when not in the credentials provider callback flow
+                return encode({ token, secret, maxAge });
+            },
+            decode: async ({ token, secret }) => {
+                if (checkIfCredentials(req)) return null;
+                // Revert to default behaviour when not in the credentials provider callback flow
+                return decode({ token, secret });
+            },
+        },
+        secret: env.NEXTAUTH_SECRET,
+        debug: true,
+        providers: [
+            GoogleProvider({
+                clientId: env.GOOGLE_CLIENT_ID,
+                clientSecret: env.GOOGLE_CLIENT_SECRET,
+                //@ts-ignore
+                profile: (profile, tokens) => {
+                    return {
+                        id: profile.sub,
+                        email: profile.email
+                    };
+                },
+            }),
+            CredentialProvider({
+                name: "Credentials",
+                credentials: {
+                    email: { label: "Email", type: "text" },
+                    password: { label: "Password", type: "password" },
+                },
+                async authorize(credentials) {
+                    if (!credentials) return null;
+                    const { email, password } = credentials;
+
+                    const user = await prisma.user.findUnique({
+                        where: {
+                            email: email,
+                        },
+                    });
+                    // TODO hash password
+                    if (!user) return null;
+                    if (user.password === null) return null;
+                    if (user.password !== password) return null;
+                    
+                    // cast profileID to number | undefined from prisma type (number | null)
+                    return { ...user, profileID: user.profileID? user.profileID : undefined };
+                },
+            }),
+        ],
+    };
+    return [req, res, opts];
+}
--- src/server/common/get-server-auth-session.ts
+++ src/server/common/get-server-auth-session.ts
@@ -1,15 +1,18 @@
-import { type GetServerSidePropsContext } from "next";
+import { NextApiRequest, NextApiResponse, type GetServerSidePropsContext } from "next";
 import { unstable_getServerSession } from "next-auth";
 
-import { authOptions } from "../../pages/api/auth/[...nextauth]";
+import { requestWrapper } from "../../pages/api/auth/[...nextauth]";
 
 /**
  * Wrapper for unstable_getServerSession https://next-auth.js.org/configuration/nextjs
  * See example usage in trpc createContext or the restricted API route
  */
+
 export const getServerAuthSession = async (ctx: {
   req: GetServerSidePropsContext["req"];
   res: GetServerSidePropsContext["res"];
 }) => {
-  return await unstable_getServerSession(ctx.req, ctx.res, authOptions);
+  // req and res passed just in case that credentials are present
+  const [a, b, options] = requestWrapper(ctx.req as NextApiRequest, ctx.res as NextApiResponse);
+  return await unstable_getServerSession(ctx.req, ctx.res, options);
 };
