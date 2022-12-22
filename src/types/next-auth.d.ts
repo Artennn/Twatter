4d17bd45304416b8f3a397b7c7949522c607dca3
Artennn <119894750+Artennn@users.noreply.github.com>
2022-12-22 23:18:51 +0100

    fix(auth): move interfaces

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,12 +1,19 @@
 import { type DefaultSession } from "next-auth";
 
 declare module "next-auth" {
+  // database user
+  interface User {
+    profileID?: number,
+    isAdmin?: boolean,
+  }
   /**
    * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
    */
   interface Session {
     user?: {
-      id: string;
-    } & DefaultSession["user"];
+      authID: string,
+      profileID?: number,
+      isAdmin?: boolean,
+    }
   }
 }
