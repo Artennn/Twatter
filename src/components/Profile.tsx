c2c9929f14cbe1618ddb81f4bf8a65115159f4c8
Artennn <119894750+Artennn@users.noreply.github.com>
2022-12-24 18:55:21 +0100

    feat(profile): verified icon

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,4 +1,4 @@
-import { Avatar, Box, Stack, Button, IconButton, Tab, Tabs, Typography } from "@mui/material";
+import { Avatar, Box, Stack, Button, IconButton, Tab, Tabs, Typography, SvgIcon } from "@mui/material";
 import { Profile as ProfileDB } from "@prisma/client";
 
 import NotificationsIcon from '@mui/icons-material/Notifications';
@@ -82,9 +82,16 @@ const Profile = ({
             {showDialog && <EditProfileDialog profile={profile} onClose={handleToggleDialog} />}
 
             <Stack direction="column" p={2} mt={3} color="text.dark">
-                <Typography fontSize={20} fontWeight={700} color="white"> {profile.displayName} </Typography>
-                <Typography variant="subtitle2" mb={1.5}> {"@" + profile.username} </Typography>
-
+                <Stack direction="row">
+                    <Typography fontSize={20} fontWeight={700} color="white"> {profile.displayName} </Typography>
+                    {profile.verified && 
+                        <SvgIcon fontSize="small" sx={{ m: "auto", ml: 0.5, color: "text.secondary" }}>
+                            <path d="M22.25 12c0-1.43-.88-2.67-2.19-3.34.46-1.39.2-2.9-.81-3.91s-2.52-1.27-3.91-.81c-.66-1.31-1.91-2.19-3.34-2.19s-2.67.88-3.33 2.19c-1.4-.46-2.91-.2-3.92.81s-1.26 2.52-.8 3.91c-1.31.67-2.2 1.91-2.2 3.34s.89 2.67 2.2 3.34c-.46 1.39-.21 2.9.8 3.91s2.52 1.26 3.91.81c.67 1.31 1.91 2.19 3.34 2.19s2.68-.88 3.34-2.19c1.39.45 2.9.2 3.91-.81s1.27-2.52.81-3.91c1.31-.67 2.19-1.91 2.19-3.34zm-11.71 4.2L6.8 12.46l1.41-1.42 2.26 2.26 4.8-5.23 1.47 1.36-6.2 6.77z"></path>
+                        </SvgIcon>
+                    }
+                </Stack>
+                <Typography variant="subtitle2" mb={2}> {"@" + profile.username} </Typography>
+                
                 {/* TODO: change to parsed text */}
                 {profile.description &&
                     <Typography fontSize={15} color="white" mb={1.5} > {profile.description} </Typography>
